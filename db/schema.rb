b166a0a9449698b7270b4af0a141c3ccfca7ab79
Yutaka Kato <p.t.p.0124@gmail.com>
2023-06-25 16:08:28 +0900

    RailsコンソールでのUser.all問題の診断

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_24_054235) do
+ActiveRecord::Schema.define(version: 2023_06_24_101057) do
 
   create_table "reservations", force: :cascade do |t|
     t.date "check_in"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2023_06_24_054235) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.text "introduction"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 

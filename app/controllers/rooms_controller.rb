a5f6bab7808692dc8d0c18421b309783d22a892d
Yutaka Kato <p.t.p.0124@gmail.com>
2023-06-26 15:05:25 +0900

    部屋の作成機能と詳細表示機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,28 @@
 class RoomsController < ApplicationController
-end
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def new
+    @room = Room.new
+  end
+  
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to @room
+    else
+      render :new
+    end
+  end  
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :description, :price, :address)
+  end
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
-  resources :rooms
+  resources :rooms, only: [:index, :show, :new, :create]
   resources :reservations
 end
